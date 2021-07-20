83669a4b1db63046c3f1f4497a202ff9b4c3b449
Pedro Bernardo <pedrovbernardo@gmail.com>
2021-07-20 13:06:06 -0300

    feat: adicionar suporte aos produtos do processo 235 (generalista)
    Categorias: Consultoria SST e Demais NR

--- dist/ProductInfo.esm.js
+++ dist/ProductInfo.esm.js
@@ -1,6 +1,118 @@
 var products = [
   {
     ids: [
+      '2.02.09.05.01.01.01'
+    ],
+    name: 'Auditoria de Conformidade às NR',
+    abbr: 'Auditoria NR',
+    category: 'Consultoria SST'
+  },
+  {
+    ids: [
+      '2.02.09.05.01.02.01'
+    ],
+    name: 'Consultoria para Adequação às NR',
+    abbr: 'Consultoria NR',
+    category: 'Consultoria SST'
+  },
+  {
+    ids: [
+      '2.02.09.05.01.05.01'
+    ],
+    name: 'Consultoria para Implantação de Sistema de Gestão em SST (ISO 45001)',
+    abbr: 'Consultoria NR - ISO 45001',
+    category: 'Consultoria SST'
+  },
+  {
+    ids: [
+      '2.02.09.05.01.06.01'
+    ],
+    name: 'Consultoria para Gestão de SST para Cadeia de Fornecedores',
+    abbr: 'Consultoria NR - Fornecedores',
+    category: 'Consultoria SST'
+  },
+  {
+    ids: [
+      '2.02.09.05.01.04.02'
+    ],
+    name: 'Consultoria em Gestão de Risco em Máquinas e Equipamentos (NR 12)',
+    abbr: 'Consultoria NR 12',
+    category: 'Demais NR'
+  },
+  {
+    ids: [
+      '2.02.09.03.01.01.13'
+    ],
+    name: 'Projeto/Laudo Técnico das Instalações Elétricas (NR 10)',
+    abbr: 'Projeto/Laudo NR 10',
+    category: 'Demais NR'
+  },
+  {
+    ids: [
+      '2.02.09.03.01.01.14'
+    ],
+    name: 'Projeto de Proteção Contra Incêndio e Pânico (NR 23)',
+    abbr: 'Projeto NR 23',
+    category: 'Demais NR'
+  },
+  {
+    ids: [
+      '2.02.09.03.01.01.15'
+    ],
+    name: 'Plano de Resposta à Emergência',
+    abbr: 'Plano de Resposta',
+    category: 'Demais NR'
+  },
+  {
+    ids: [
+      '2.02.09.03.01.01.17'
+    ],
+    name: 'Programa de Proteção Respiratória - PPR',
+    abbr: 'PPR',
+    category: 'Demais NR'
+  },
+  {
+    ids: [
+      '2.02.09.03.01.04.01'
+    ],
+    name: 'Realização do Inventário, Plano de Adequação e cronograma das máquinas e equipamentos de acordo com a NR 12',
+    abbr: 'Inventário/Plano NR 12',
+    category: 'Demais NR'
+  },
+  {
+    ids: [
+      '2.02.09.03.01.04.02'
+    ],
+    name: 'Elaboração do manual de máquinas e equipamentos - NR 12',
+    abbr: 'Manual de Máquinas NR 12',
+    category: 'Demais NR'
+  },
+  {
+    ids: [
+      '2.02.09.03.01.04.03'
+    ],
+    name: 'Elaboração de laudo técnico de conformidade - NR 12',
+    abbr: 'Laudo NR 12',
+    category: 'Demais NR'
+  },
+  {
+    ids: [
+      '2.02.01.03.01.01.19'
+    ],
+    name: 'Projeto Sistema de Proteção contra Descargas Atmosféricas (SPDA) - NR 10',
+    abbr: 'SPDA NR 10',
+    category: 'Demais NR'
+  },
+  {
+    ids: [
+      '2.02.01.03.01.01.18'
+    ],
+    name: 'Análise e Investigação de Acidentes',
+    abbr: 'Investigação de Acidentes',
+    category: 'Demais NR'
+  },
+  {
+    ids: [
       '2.02.09.04.01.01.01'
     ],
     name: 'Programa de Gerenciamento de Riscos (PGR) - NR 01',
