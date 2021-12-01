cc6743e5dad15a209a519843c3c6036b0659450f
Pedro Bernardo <pedrovbernardo@gmail.com>
2021-12-01 16:58:47 -0300

    chore: ajustar builds com rollup e package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,17 @@
+import pkg from './package.json'
+
 export default {
   input: './src/index.js',
   output: [
     {
-      name: 'ProductInfo',
-      file: 'dist/ProductInfo.js',
-      format: 'umd',
-      sourcemap: true
-    },
-    {
-      name: 'ProductInfo',
-      file: 'dist/ProductInfo.esm.js',
+      file: pkg.module,
       format: 'esm',
-      exports: 'named',
       sourcemap: true
     },
     {
       name: 'ProductInfo',
-      file: 'dist/ProductInfo.cjs.js',
-      format: 'cjs',
-      exports: 'named',
+      file: pkg.browser,
+      format: 'umd',
       sourcemap: true
     }
   ]
